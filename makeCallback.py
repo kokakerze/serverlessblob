d01455acfbd569facd53b8ca749db0f80abdfd5c
KokaKerze <mr.kertsman@gmail.com>
2021-07-27 01:39:35 +0300

    make callBack with DB Stream

--- makeCallback.py
+++ makeCallback.py
@@ -1,19 +1,18 @@
 import os
 
 import boto3
-import simplejson as json
 from boto3.dynamodb.conditions import Key
 
 
-def lambda_handler(event, context):
-    dynamodb = boto3.resource('dynamodb')
+def lambda_handler(event, context, dynamodb=None):
+    pid = event["Records"][0]['dynamodb']['NewImage']['pid']['S']
+
+    if not dynamodb:
+        dynamodb = boto3.resource('dynamodb')
+
     table_name = os.environ.get('BLOBS_TABLE')
     table = dynamodb.Table(table_name)
-    order_id = event['pathParameters']['id']
-    response = table.query(KeyConditionExpression=Key('pid').eq(order_id))
-
-    return {
-        "statusCode": 200,
-        "headers": {},
-        "body": json.dumps(response['Items'])
-    }
+    response = table.query(
+        KeyConditionExpression=Key('pid').eq(pid)
+    )
+    return response['Items']
